7efd71711ccf38f10f51ee0bf733d9ba6e77666a
Evgeny Blokhin <eb@tilde.pro>
2015-11-11 15:26:23 +0100

    add tests and aux files

---
+++ src/test_matinfio.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var MatinfIO = require('./js/libs/MatinfIO');
+
+if (process.argv.length < 3){
+    console.error("Path/file must be given.");
+    process.exit();
+}
+
+var walk = function(dir, done){
+    var results = [];
+    fs.readdir(dir, function(err, list){
+        if (err) return done(err);
+        var i = 0;
+        (function next(){
+            var file = list[i++];
+            if (!file) return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function(err, stat){
+                if (stat && stat.isDirectory()){
+                    walk(file, function(err, res){
+                    results = results.concat(res);
+                    next();
+                    });
+                } else {
+                    results.push(file);
+                    next();
+                }
+            });
+        })();
+    });
+};
+
+var examine = function(file){
+    fs.readFile(file, function(err, data){
+        if (err) {
+        return console.log(err);
+        }
+        console.log("======================");
+        console.log(file);
+        console.log("======================");
+        console.log(MatinfIO.to_flatten(data));
+    });
+};
+
+var target = process.argv[process.argv.length-1];
+
+if (fs.lstatSync(target).isDirectory()){
+    walk(target, function(err, results){
+        if (err) throw err;
+        results.forEach(examine);
+    });
+} else examine(target);
