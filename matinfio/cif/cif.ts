6a98badeaac4fd42c235bfec96f6ea6a6441f8b1
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2024-01-13 18:39:27 +1100

    remove `startsWith` polyfill

--- matinfio/cif/cif.ts
+++ matinfio/cif/cif.ts
@@ -2,15 +2,6 @@ namespace $ {
 
 	const math = $mpds_cifplayer_lib_math
 
-	if( !String.prototype.startsWith ) {
-		Object.defineProperty( String.prototype, 'startsWith', {
-			value: function( search: string, rawPos: number ) {
-				var pos = rawPos > 0 ? rawPos|0 : 0;
-				return this.substring( pos, pos + search.length ) === search;
-			}
-		});
-	}
-
 	export function $mpds_cifplayer_matinfio_cif_to_obj( this: $, str: string ): $mpds_cifplayer_matinfio_internal_obj {
 		const structures = []
 		let symops = []
